61a9fb98d51e373ab87d8e8d720cdade1031bea5
Ava <reacthmd34@gmail.com>
2023-11-15 16:55:51 +0100

    Auth0 - Setup

--- src/index.js
+++ src/index.js
@@ -5,12 +5,22 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { GithubProvider } from "./context/context";
 import { Auth0Provider } from "@auth0/auth0-react";
+//dev-akqfa7r502o7iv4z.us.auth0.com
+//n4PuBrg3J0JMQVZ4zXE0ag1USXN9Etay
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <GithubProvider>
-      <App />
-    </GithubProvider>
+    <Auth0Provider
+      omain="dev-akqfa7r502o7iv4z.us.auth0.com"
+      clientId="n4PuBrg3J0JMQVZ4zXE0ag1USXN9Etay"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>
 );
 
